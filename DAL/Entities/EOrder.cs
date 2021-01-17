98226e197b9e07f788da4a279964b8c3b6d054e8
Keith Lai <k34lai@uwaterloo.ca>
2021-01-17 15:44:06 -0500

    Add files via upload

---
+++ DAL/Entities/EOrder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class EOrder
+    {
+        public bool DbError { get; set; }
+        public int OrderNumber { get; set; }
+        public int CustomerNumber { get; set; }
+        public string OrderDate { get; set; }
+        public string ShippingAddress { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+        public int RecordCount { get; set; }
+    }
+}
